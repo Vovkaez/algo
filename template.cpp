24472e45626d6fa1488ae87c6aa8f9f97693eb50
Vova <mandarin123573@gmail.com>
2020-07-25 21:13:50 +0300

    Codeforces template

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+
+using namespace std;
+const long long LINF = LLONG_MAX;
+const int INF = INT_MAX;
+const int MOD = 1000000007;
+
+const ld PI = 3.14159265358979323;
+
+const int N = 4e5 + 111;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
