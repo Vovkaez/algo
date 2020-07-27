ae0328abd5ebcc49b7574d5e74dd54d93347c57f
Vova <mandarin123573@gmail.com>
2020-07-28 00:30:31 +0300

    Codeforces template

--- segtree.cpp
+++ segtree.cpp
@@ -4,14 +4,6 @@
 #define ld long double
 
 using namespace std;
-const long long LINF = LLONG_MAX;
-const int INF = INT_MAX;
-const int MOD = 1000000007;
-
-const ld PI = 3.14159265358979323;
-
-const int N = 1e6 + 111;
-int n;
 
 template<typename T>
 struct segtree{
@@ -54,12 +46,3 @@ struct segtree{
         set(i, x, (j << 1) + 1, m + 1, rx);
     }
 };
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cin >> n;
-    vector<ll> a(n);
-    for (auto& x : a)
-        cin >> x;
-}
